2965a705af236035c0cf664b5fa149d83b8016b9
Ali Mansour <dev.ali.mansour@gmail.com>
2021-09-06 22:36:49 +0200

    Update gradle dependencies. Put google-services.json file in app directory to link the app with Firebase project. Enable MultiDex for the app. Enable DataBinding for activity_reminders.xml. Remove deprecated 'kotlin-android-extensions' plugin from build.gradle file and use DataBinding instead. Replace fragment with FragmentContainerView in activity_reminders.xml.

--- starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,31 +1,33 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRemindersBinding
+    private lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reminders)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                navController.popBackStack()
                 return true
             }
         }
